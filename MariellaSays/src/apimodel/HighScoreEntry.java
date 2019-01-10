266a25a811a672ce5e6ef535db6c3dd9f40ca833
Unknown <bad_hunter_666@yahoo.it>
2019-01-10 17:11:31 +0100

    fox back -^^,--,~"

--- MariellaSays/src/apimodel/HighScoreEntry.java
+++ MariellaSays/src/apimodel/HighScoreEntry.java
@@ -1,34 +1,34 @@
-package apimodel;
-
-public class HighScoreEntry {
-
-	private int score;
-	private String nickname;
-	
-
-	public HighScoreEntry() {
-
-	}
-
-	
-	public HighScoreEntry(int score, String nickname) {
-		this.score = score;
-		this.nickname = nickname;
-	}
-
-	public int getScore() {
-		return score;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return nickname + ":" + score;
-	}
-	
+package apimodel; 
+ 
+public class HighScoreEntry { 
+ 
+	private int score; 
+	private String userName; 
+	 
+ 
+	public HighScoreEntry() { 
+ 
+	} 
+ 
+	 
+	public HighScoreEntry(int score, String nickname) { 
+		this.score = score; 
+		this.userName = nickname; 
+	} 
+ 
+	public int getScore() { 
+		return score; 
+	} 
+ 
+	public String getNickname() { 
+		return userName; 
+	} 
+ 
+ 
+ 
+	@Override 
+	public String toString() { 
+		return userName + ":" + score; 
+	} 
+	 
 }
